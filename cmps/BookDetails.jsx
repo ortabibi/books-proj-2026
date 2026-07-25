0dcc83fa7dd5c283087fda58c272f3f395ac1d37
Or <ortabibi2002@gmail.com>
2026-07-25 16:56:51 +0300

    price color feature done

--- cmps/BookDetails.jsx
+++ cmps/BookDetails.jsx
@@ -9,12 +9,20 @@ export function BookDetails({ selectedBook, onCloseDetails }) {
         else dialogRef.current.close()
     }, [selectedBook])
 
+    function getPriceClass(amount) {
+        if (amount > 150) return 'red'
+        if (amount < 20) return 'green'
+        return ''
+    }
 
     return <dialog onClose={onCloseDetails} ref={dialogRef} closedby="any" className="book-details">
         <img src={selectedBook && selectedBook.thumbnail} alt="" />
-        <h2>{selectedBook && selectedBook}</h2>
-        <p>{selectedBook && selectedBook}</p>
-        <p>{selectedBook && selectedBook}</p>
+        <h2>{selectedBook && selectedBook.title}</h2>
+        <p className={selectedBook ? getPriceClass(selectedBook.listPrice.amount) : ''}
+        >{selectedBook && selectedBook.listPrice.amount}
+        </p>
+        <p>{selectedBook && selectedBook.publishedDate}</p>
+        <p>{selectedBook && selectedBook.pageCount}</p>
 
         <button onClick={onCloseDetails}>x</button>
     </dialog>
