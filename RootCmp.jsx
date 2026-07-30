8224064910340446d8c781afc4ae4fbb8d63b06b
Or <ortabibi2002@gmail.com>
2026-07-30 11:41:27 +0300

    new route logic

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,21 +1,26 @@
-const { useState } = React
-
+const { Routes, Route, HashRouter: Router } = ReactRouterDOM
 import { AppHeader } from "./cmps/AppHeader.jsx"
+
 import { Home } from "./pages/Home.jsx"
 import { About } from "./pages/About.jsx"
 import { BookIndex } from "./pages/BookIndex.jsx"
 
 
 export function RootCmp() {
-    const [page, setPage] = useState('BookIndex')
 
-    return <section className="app main-layout">
-        <AppHeader page={page} onSetPage={setPage} />
-
-        <main>
-            {page === 'Home' && <Home />}
-            {page === 'About' && <About />}
-            {page === 'BookIndex' && <BookIndex />}
-        </main>
-    </section>
+    return (
+        <Router>
+            <section className="app main-layout">
+                <AppHeader />
+                {/* <UserMsg /> */}
+                <main>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/" element={<About />} />
+                        <Route path="/" element={<BookIndex />} />
+                    </Routes>
+                </main>
+            </section>
+        </Router>
+    )
 }
